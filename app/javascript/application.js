3d5e7f1a81b4693ba0a22b3c7d8d6323ac44619e
court-tek <dishoughng@gmail.com>
2023-11-06 14:04:37 -0500

    changed line height of header menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "./src/header"
 
-console.log("hello nurse");
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-# config/importmap.rb
 pin_all_from "app/javascript/src", under: "src", to: "src"
