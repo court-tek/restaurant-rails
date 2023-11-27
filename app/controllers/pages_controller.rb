70b0537a5e0a95e6f0e7ebb86d9e1dbad93eae19
court-tek <dishoughng@gmail.com>
2023-11-27 18:01:16 -0330

    added index method to Pages Controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+    def index
+        @menu_categories = MenuCategory.all
+    end
+
     def promotions 
         @promos = ["tastemakers", "thanksgiving", "holiday lunch booking special", "our classic menu"]
     end
