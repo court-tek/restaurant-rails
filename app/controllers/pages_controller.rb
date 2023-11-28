d64814863e9ea16c4d57dc0c2ed332271f4cc1c0
court-tek <dishoughng@gmail.com>
2023-11-28 19:18:50 -0330

    added menu items .erb file to pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
     def index
         @menu_categories = MenuCategory.all
+        @menu_items = MenuItem.joins(:menu_category)
     end
 
     def promotions 
